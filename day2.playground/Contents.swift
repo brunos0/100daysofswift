7b8ecf51b88f8a31b1f73064f7a3416b5d65b442
Bruno Horvat <brunoso@gmail.com>
2024-04-22 18:59:41 -0300

    day 2

--- day2.playground/Contents.swift
+++ day2.playground/Contents.swift
@@ -71,3 +71,59 @@ staff["001"] = "Paulo"
 var staff2 = Dictionary<Int,String>()
 staff2[1] = "Pedro"
 
+//enums
+enum  Result  {
+    case failure
+    case success
+}
+var process = Result.failure
+process == Result.failure
+
+//enums com valores associados
+enum Atividade {
+    case entendiado
+    case conversando(Assunto: String)
+    case cantando(musica: String)
+    case arrecadando(valor: Double)
+}
+
+var realizado = Atividade.arrecadando(valor: 20.1)
+var proza = Atividade.conversando(Assunto: "Fofoca")
+
+// nao focar agora nisso - mas esta aqui um exemplo de como obter os dados dentro do enum
+if case let .conversando(Assunto) = proza {
+    Assunto
+}
+
+//enums com valores 'crus'
+
+enum Games: Int{
+    case Playstation
+    case Xbox
+    case Switch
+    case Dreamcast
+}
+
+var preference = Games(rawValue : 3) // Dreamcast pq comeca com 0
+
+//pode ser forçar a começar com 1, atribuindo o valor 1 para a primeira opção
+
+enum Games2: Int{
+    case Playstation = 1
+    case Xbox
+    case Switch
+    case Dreamcast
+}
+
+var preference2 = Games2(rawValue : 4) // Agora Dreamcast eh o quarto item
+
+//eh possivel atribuir qualquer valor para o enum e buscar por ele mesmo que os items esteja fora de ordem
+enum Games3: Int{
+    case Playstation = 1
+    case Xbox = 7
+    case Switch = 5
+    case Dreamcast = 99
+}
+
+var preference3 = Games3(rawValue : 99) // Agora Dreamcast eh o quarto item
+

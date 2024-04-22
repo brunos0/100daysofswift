e67282c885a35072d22e76f2a1c90495dafea8a1
Bruno Horvat <brunoso@gmail.com>
2024-04-22 16:02:53 -0300

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var str = "teste"
+str = "agora é assim"
+//str = 68
+var str1 =
+"""
+a b
+c d
+e f
+"""
+var str2 =
+"""
+a b \
+c d \
+e f
+"""
+var gigantValue = 8_000_000_000_000_000_000
+var gigantValue2 = 8000000
+var isGood = true
+var pi = 3.14
+var cidade = "Paris"
+var string = "Quero ir para \(cidade)"
+let string2 = "constante"
+var nome: String = "Bruno"
+var idade: Int = 42
+var pcd: Bool = false
+var peso: Double = 89.6
+let sexo: String = "Masculino"
